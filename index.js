5f5d9a514d3f969ef9cecf33f1bfd55fffca5090
Bilal-12-2 <91077424+Bilal-12-2@users.noreply.github.com>
2022-03-01 22:57:58 +0500

    chfht

---
+++ index.js
@@ -0,0 +1,11 @@
+const myObject = {
+    firstName : "MIAN",
+    secondName : "MUHAMMAD",
+    lastName : "BILAL",
+    fullName : function(){
+    return this.firstName + this.secondName + this.lastName;
+    }
+    
+    
+    }
+    document.getElementById("demo").innerHTML = myObject.fullName(); 
